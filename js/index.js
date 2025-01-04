62278b254964006aeebe11d37327c27a47fca7d1
Altamir Rodrigues <box.altamir@gmail.com>
2025-01-04 17:18:12 +0000

    Correção dos linksExplorer do Index

---
+++ js/index.js
@@ -0,0 +1,13 @@
+
+      document.getElementById('explorerJapan').addEventListener('click', function() {
+        window.location.href = 'detalhesPais.html?country=JPN'; // Japão
+      });
+
+      document.getElementById('explorerAustralia').addEventListener('click', function() {
+        window.location.href = 'detalhesPais.html?country=AUS'; // Austrália (Sidney)
+      });
+
+      document.getElementById('explorerItaly').addEventListener('click', function() {
+        window.location.href = 'detalhesPais.html?country=ITA'; // Itália (Roma)
+      });
+  
