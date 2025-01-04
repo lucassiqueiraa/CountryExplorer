6b75606f1677ada22f89a3a640c5720dc12318f7
Altamir Rodrigues <box.altamir@gmail.com>
2025-01-04 16:37:18 +0000

    correções de footer e navbar

--- js/footer.js
+++ js/footer.js
@@ -1,12 +1,3 @@
-/*document.getElementById("footer-placeholder").innerHTML = fetch("footer.html")
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById("footer-placeholder").innerHTML = data;
-    })
-    .catch(error => {
-        console.error('Erro ao carregar o footer: ', error);
-    });
-    */
 const xhr = new XMLHttpRequest();
 xhr.open("GET", "footer.html", true);
 
