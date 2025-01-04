68a3e5a0bc110279f00c397d5a460c5f9980b693
Altamir Rodrigues <box.altamir@gmail.com>
2025-01-04 10:04:54 +0000

    Ajustando de fetch para ajax.

--- js/footer.js
+++ js/footer.js
@@ -1,4 +1,4 @@
-document.getElementById("footer-placeholder").innerHTML = fetch("footer.html")
+/*document.getElementById("footer-placeholder").innerHTML = fetch("footer.html")
     .then(response => response.text())
     .then(data => {
         document.getElementById("footer-placeholder").innerHTML = data;
@@ -6,3 +6,20 @@ document.getElementById("footer-placeholder").innerHTML = fetch("footer.html")
     .catch(error => {
         console.error('Erro ao carregar o footer: ', error);
     });
+    */
+const xhr = new XMLHttpRequest();
+xhr.open("GET", "footer.html", true);
+
+xhr.onload = function () {
+    if (xhr.status === 200) {
+        document.getElementById("footer-placeholder").innerHTML = xhr.responseText;
+    } else {
+        console.error(`Erro ao carregar o footer: ${xhr.statusText}`);
+    }
+};
+
+xhr.onerror = function () {
+    console.error("Erro na requisição AJAX para o footer");
+};
+
+xhr.send();
